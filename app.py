82b534ecc7642c9dac14eb38bd9f4b2b0d807661
aashrith2000 <59306806+aashrith2021@users.noreply.github.com>
2023-03-13 18:43:07 +0000

    Added features

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru',
+    'salary': '20'
+  },
+  {
+    'id': 2,
+    'title': 'Data Engineer',
+    'location': 'Noida',
+    'salary': '25'
+  },
+  {
+    'id': 3,
+    'title': 'Data Scientist',
+    'location': 'Hyderabad',
+    
+  },
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs = JOBS, company_name = 'Aashrith')
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True)
