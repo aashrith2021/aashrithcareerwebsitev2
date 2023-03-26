9cd9efc7cfebbb55a7baee4fa7a0360f9a891eec
aashrith2000 <59306806+aashrith2021@users.noreply.github.com>
2023-03-26 18:09:11 +0000

    Added dynamic pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 app = Flask(__name__)
 
 
@@ -8,8 +8,16 @@ app = Flask(__name__)
 def hello_aashrith():
   jobs = load_jobs_from_db()
   return render_template('home.html',
-                         jobs = jobs,
-                         company_name = 'Aashrith')
+                         jobs = jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html',
+                        job = job)
+
 
 @app.route("/api/jobs")
 def list_jobs():
